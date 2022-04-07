152ea98d95fc282efe8ec76ecdf7eaea12e9e257
udayprsingh <udayprsingh@deloitte.com>
2022-04-07 21:28:43 +0530

    user can choose movie

--- Main_assignment/UserRegistration.py
+++ Main_assignment/UserRegistration.py
@@ -1,15 +1,17 @@
 from openpyxl import load_workbook
 
+
 class register:
     def userregister(self):
             username = input('Enter username: ')
             if username in open('register.txt', 'r').read():
                 print('Username already exists')
-                exit()
+                return False
             password = input('Enter password: ')
             c_password = input('Enter confirm password: ')
             if password != c_password:
                 print('password did not match')
+                return False
             file = open('register.txt', 'a')
             file.write(username)
             file.write(' ')
@@ -17,11 +19,12 @@ class register:
             file.write('\n')
             file.close()
             print('You are successfully registered ')
+            return True
 
-    def login(self):
-        print("Login to BookMyShow: ")
-        username = input('Enter username: ')
-        password = input('Enter password: ')
+    def userlogin(self):
+        print("****Login to BookMyShow: ****")
+        username = input('Enter registered username: ')
+        password = input('Enter registered password: ')
         get_data = open('register.txt', 'r').readlines()
         users_data = []
         for user in get_data:
@@ -38,7 +41,18 @@ class register:
             increment += 1
 
         if login_success == 1:
-            print('Welcome ' + username)
+            print("Login Successful Welcome",username)
+            return True
         else:
-            print('invalid username & password')
-        exit()
+            print('invalid username or password ')
+            return False
+
+    def movies(self):
+        print("*** Movies List ***")
+        wb = load_workbook("tim.xlsx")
+        ws = wb.active
+        rows = len(list(ws.rows))
+        range = ws["A1":"A" + str(rows)]
+        for movie in range:
+            for x in movie:
+                print(x.value)
